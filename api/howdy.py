37d42eb015b87ec311befaea1d6bdbcd8fa2490b
JB-Tellez <jb.tellez@gmail.com>
2022-05-09 11:28:44 -0700

    fixes howdy

--- api/howdy.py
+++ api/howdy.py
@@ -1,14 +1,14 @@
 from http.server import BaseHTTPRequestHandler
 from datetime import datetime
 
+
 class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
 
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
+        message = "Howdy"
 
-    message = "Howdy"
-    
-    self.wfile.write(message.encode())
-    return
+        self.wfile.write(message.encode())
+        return
